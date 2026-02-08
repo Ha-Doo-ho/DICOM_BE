a606665a3f8d918f6ba2b31b003b1c138760f912
Ha-doo-ho <haduho4339@gmail.com>
2026-02-08 21:44:18 +0900

    skeleton code and document

--- src/main/java/com/MedLab/controller/DicomController.java
+++ src/main/java/com/MedLab/controller/DicomController.java
@@ -2,10 +2,16 @@ package com.MedLab.controller;
 
 import lombok.Getter;
 import org.apache.coyote.Response;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -51,7 +57,22 @@ public class DicomController {
         response.put("id", id);
         response.put("fileName", "test_image.dcm");
         response.put("status", "SUCCESS");
+
+        //front-end 다운로드 버튼이 호출할 API 주소를 미리 알려줌.
+        response.put("downloadUrl", "/api/dicom/download/" + id);
         return ResponseEntity.ok(response);
+    }
+
+    //파일 다운로드 엔드포인트
+    @GetMapping("/download/{id}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable int id) {
+        Path path = Paths.get("/storage/png/test_result.png");
+        Resource resource = new FileSystemResource(path);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_PNG)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachmentl filename=\"converted_image.png\"")
+                .body(resource);
     }
 
 }
