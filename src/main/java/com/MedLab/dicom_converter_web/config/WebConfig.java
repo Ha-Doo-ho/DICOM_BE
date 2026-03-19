d672f788a09fe3cecf4b70a5d11262c1b6c2b618
Ha doo ho <Haduho4339@gmail.com>
2026-03-19 19:43:53 +0900

    Update CORS configuration in WebConfig.java

--- src/main/java/com/MedLab/dicom_converter_web/config/WebConfig.java
+++ src/main/java/com/MedLab/dicom_converter_web/config/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 경로에 대해
                 //.allowedOriginPatterns("*") //  모든 도메인 (출처) 허용
                 //.allowedOrigins("http://localhost:3000") // 프론트엔드 서버 주소 (React 디폴트값)
-                .allowedOrigins("http://localhost:8080","http://127.0.0.1:5500", "http://localhost:5500","http://localhost:63343") // 실제 프론트엔드 주소를 명시)
+                .allowedOrigins("http://localhost:8080","http://127.0.0.1:5500", "http://localhost:5500","http://localhost:63343")
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키나 인증정보 허용
@@ -37,3 +37,5 @@ public class WebConfig implements WebMvcConfigurer {
 }
 
 //주의 : allowedOrigins("*")와  allowCredentials(true)는 동시에 쓸 수 없음. 반드시 allowedOriginPatterns("*")를 사용할 것.
+// allowCredentials(true) : 요청에 사용자의 인증 정보(쿠키, 세션 등...)을 포함해서 보내는 것을 허락한다.
+// alloedOrigins("*") 모든 웹사이트(악성 포함에서 나의 서버로 요청을 보내도 다 받음.
